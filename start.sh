844b150139ed2dd4d04a2b11b455f35b4dc93d49
makerspace <mail@bodensee-gymnasium.de>
2023-07-19 13:51:26 +0200

    Added start scripts

---
+++ start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+tmux new-session -d -s arcade './start_arcade.sh'
---
+++ start_arcade.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export DISPLAY=:0
+cd ~/PycharmProjects/MakerSpaceArcade
+source venv/bin/activate
+python main.py
